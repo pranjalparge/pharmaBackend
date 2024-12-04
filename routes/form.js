b651597013519190d2a3121399411bd7fb3d9c10
pranjalparge <pranjalparge201@gmail.com>
2024-12-04 14:55:50 +0530

    dsff

--- routes/form.js
+++ routes/form.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const { addForm, login, home,contactUs } = require('../controller/job_apply_form');
+const { addForm,  } = require('../controller/job_apply_form');
 
 const { protect } = require('../auth_middleware');
 const router = express.Router();
 
 router.post('/addForm', addForm); // protect,
-
-router.post('/contactUs', contactUs); // protect,
-router.post('/login', login);
-router.get('/', home);
 
 
 
